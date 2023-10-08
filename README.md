cd7bb2a07403d276bb4e4fb3e655811a52d28085
Wellington Brandão <wellington.brandao@simbiox.com.br>
2023-10-07 22:00:00 -0300

    feat: create service for consumer information

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+## Front feito no NextJs responsável por consumir informações de temperatura de uma fila no Azure Service Bus
+
+![Alt text](image.png)
+
+
+
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- src/utils/service.ts
+++ src/utils/service.ts
@@ -1,8 +1,10 @@
 const { ServiceBusClient } = require("@azure/service-bus");
 
-export async function getTemperature() {
-  const serviceBusClient = new ServiceBusClient("<connectionString>");
-  const receiver = serviceBusClient.createReceiver("my-queue");
+export  async function getTemperature() {
+  const serviceBusClient = new ServiceBusClient(process.env.NEXT_PUBLIC_CONNECTION);
+  const receiver = serviceBusClient.createReceiver("temperature");
   const myMessages = await receiver.receiveMessages(10);
-  return myMessages;
+  const temperatureValue = myMessages[0].body
+  console.log(myMessages)
+  return temperatureValue;
 }
